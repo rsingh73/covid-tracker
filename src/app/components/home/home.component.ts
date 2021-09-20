5f9e26d834cf1c8d6c072b31c0afb2c120d508d4
Ravi Kant Singh <ravi.kant19@yahoo.com>
2021-09-20 13:33:59 +0530

    Home component updated and data filtered

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSummary } from 'src/app/models/globalDataModel';
 import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
@@ -8,11 +9,28 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 })
 export class HomeComponent implements OnInit {
 
+  totalConfirmedCases = 0;
+  totalActiveCases = 0;
+  totalDeaths = 0;
+  totalRecoveredCases = 0;
+  globalData: any;
+
   constructor(private service: DataServiceService) { }
 
   ngOnInit(): void {
     this.service.getCovidData().subscribe(response => {
-      console.log(response);
+      // console.log(response);
+      this.globalData = response;
+      console.log(this.globalData)
+      response.forEach((cs: any) => {
+        if (!Number.isNaN(cs.confirmed)) {
+          this.totalActiveCases+= cs.active;
+        this.totalConfirmedCases+= cs.confirmed;
+        this.totalDeaths+= cs.deaths;
+        this.totalRecoveredCases+= cs.recovered;
+        }
+
+      })
 
     })
   }
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -2,20 +2,51 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { map } from 'rxjs/operators'
+import { DataSummary } from '../models/globalDataModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  private url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-12-2021.csv';
+  private url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-19-2021.csv';
   constructor(private http: HttpClient) { }
 
   getCovidData() {
     return this.http.get(this.url, {responseType: 'text'}).pipe(
       map(response => {
-        // console.log(response)
-        return response
+        let data: DataSummary[] = [];
+        let raw: any = {};
+        let rows = response.split("\n");
+        rows.splice(0, 1);
+        // console.log(rows)
+        rows.forEach(row => {
+          let cols = row.split(/,(?=\S)/)
+
+          let cs: any = {
+            country: cols[3],
+            confirmed: +cols[7],
+            deaths: +cols[8],
+            recovered: +cols[9],
+            active: +cols[10]
+          }
+          let temp: DataSummary = raw[cs.country]
+          if (temp) {
+            temp.active = cs.active + temp.active
+            temp.confirmed = cs.confirmed + temp.confirmed
+            temp.deaths = cs.deaths + temp.deaths
+            temp.recovered = cs.recovered + temp.recovered
+
+            raw[cs.country] = temp;
+          } else {
+            raw[cs.country] = cs
+          }
+
+
+        })
+        // console.log(raw);
+        return <DataSummary[]>Object.values(raw);
+        // return Object.values(raw);
       })
     )
   }
